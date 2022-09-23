d5d82bbb8e90c631cc260e692abb02c894fc021d
Paulo Gomes <pjgomes85@hotmail.com>
2022-09-23 14:39:02 +0100

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_112344) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_29_130141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_112344) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "rating"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
